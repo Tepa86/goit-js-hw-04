dddc9fab6204d9d1413a8ba3407f13ba3cb76fdb
Tepa86 <teplitskiy86@gmail.com>
2023-11-17 15:52:31 +0200

    addTusk

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,29 @@
-function slugify(title) {
-    const slug = title.toLowerCase().split(" ");
-    
-    return slug.join("-")
-
+function isEnoughCapacity(products, containerSize) {
+let totalProd = 0;
+    const value = Object.values(products)
+    for (let val of value) {
+        totalProd+=val;
+    }
+    if (totalProd<=containerSize) {
+        return true
+    }
+return `false`
 }
 
 
 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
